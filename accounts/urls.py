be6c681e78b6944f1c4078a28c3556302084c57f
MohammdSobhanSt <habibdokhtmohammadsobhan@gmail.com>
2026-05-10 14:15:00 +0330

    Added PasswordChange.

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
-
+from django.contrib.auth import views as auth_views
+from django.urls import reverse_lazy
 
 app_name = "accounts"
 urlpatterns = [
@@ -10,4 +11,15 @@ urlpatterns = [
     path('profile/', views.UserProfileView.as_view(), name='user-profile'),
     path('profile/edit/', views.UserProfileEditView.as_view(), name='edit-profile'),
     path('profile/delete/', views.UserDeleteView.as_view(), name='user-delete'),
+    path('profile/edit/password/', 
+         auth_views.PasswordChangeView.as_view(
+             template_name="accounts/password_change.html",
+             success_url=reverse_lazy('accounts:password_change_done')
+         ), 
+         name='password_change'),
+    path('profile/edit/password/done/', 
+         auth_views.PasswordChangeDoneView.as_view(
+             template_name="accounts/password_change_done.html"
+         ), 
+         name='password_change_done'),
 ]
